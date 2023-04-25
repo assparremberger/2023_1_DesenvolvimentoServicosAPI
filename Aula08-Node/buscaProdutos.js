75817429db15127d10fe99428109f91c5ca90e6b
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-04-25 12:05:59 -0300

    Aula08 - 25/04/2023 - NodeJS

---
+++ Aula08-Node/buscaProdutos.js
@@ -0,0 +1,36 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+var mysql = require("mysql");
+var conn = mysql.createConnection({
+    host : "localhost" ,
+    user : "root" ,
+    password : "" ,
+    database : "loja"
+});
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type' , 'text/plain');
+
+    conn.connect( function(erro){
+        if( !erro ){
+            sql = "SELECT * FROM produto ORDER BY nome";
+            conn.query( sql , function(err, result, fields){
+                if( !err ){
+                    res.end( JSON.stringify(result) );
+                }else{
+                    res.end( err );
+                }
+            });
+        }
+    });
+});
+
+
+server.listen( port , hostname, ()=> {
+    console.log("Servidor executando em: http://" + 
+        hostname + ":"+port+"/");
+});
