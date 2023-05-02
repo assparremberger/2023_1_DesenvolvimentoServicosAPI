c42cacde211bff2cfa49dfecbdc0d9119b6ae956
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-05-02 11:51:54 -0300

    Aula09 - 02/05/2023 - REST

---
+++ Aula09-REST/api/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+var produtos = ["Coca-cola", "Fanta", "Pepsi"];
+
+app.get('/', (req, res) => {
+    res.status(200).send('Bem-vindo ao API REST');
+} );
+
+app.get('/produtos', (req, res) => {
+    res.status(200).send( produtos );
+} );
+
+app.get('/produtos/:posicao', (req, res) => {
+    const linha = parseInt( req.params.posicao );
+    res.status(200).send( produtos[linha] );
+} );
+
+app.post('/produtos', (req, res) => {
+    var nome = "Nome " + produtos.length;
+    produtos.push(  nome );
+    res.status(200).send( "Produto adicionado");
+});
+
+http.createServer(app).listen( 8001 , () => {
+    console.log("Servidor iniciado em http://localhost:8001");
+});
