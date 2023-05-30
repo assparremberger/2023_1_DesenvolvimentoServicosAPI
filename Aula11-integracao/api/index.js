8f45b0bac1370857801514c85238dd307dbad53e
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-05-30 12:09:35 -0300

    Aula11 Integrãção Front com API

---
+++ Aula11-integracao/api/index.js
@@ -0,0 +1,89 @@
+const restify = require("restify");
+const errors = require("restify-errors");
+
+const servidor = restify.createServer({
+    name : 'Lojinha' ,
+    version : '1.0.0'
+});
+
+servidor.use( restify.plugins.acceptParser(servidor.acceptable) );
+servidor.use( restify.plugins.queryParser() );
+servidor.use( restify.plugins.bodyParser() );
+
+servidor.listen(8001, function(){
+    console.log("%s executando em %s", servidor.name, servidor.url);
+} );
+
+
+var knex = require('knex')({
+    client : 'mysql' ,
+    connection : {
+        host : 'localhost' ,
+        user : 'root' ,
+        password : '' ,
+        database : 'loja'
+    }
+});
+
+servidor.get( '/' , (req, res, next) => {
+    res.send('Bem-vindo(a) `a API Loja!');
+});
+
+servidor.get( '/produtos' , (req, res, next) => {
+    knex('produto').then( (dados) =>{
+        res.send( dados );
+    }, next) ; 
+});
+
+servidor.get( '/produtos/:idProd' , (req, res, next) => {
+    const idProduto = req.params.idProd;
+    knex('produto')
+        .where( 'id' , idProduto)
+        .first()
+        .then( (dados) =>{
+            if( !dados || dados =="" ){
+                return res.send(
+                    new errors.BadRequestError('Produto não encontrado'));
+            }
+            res.send( dados );
+        }, next) ; 
+});
+
+servidor.post( '/produtos' , (req, res, next) => {
+    knex('produto')
+        .insert( req.body )
+        .then( (dados) =>{
+            res.send( dados );
+        }, next) ; 
+});
+
+servidor.put( '/produtos/:idProd' , (req, res, next) => {
+    const idProduto = req.params.idProd;
+    knex('produto')
+        .where( 'id' , idProduto)
+        .update( req.body )
+        .then( (dados) =>{
+            if( !dados ){
+                return res.send(
+                    new errors.BadRequestError('Produto não encontrado'));
+            }
+            res.send( "Produto Atualizado" );
+        }, next) ; 
+});
+
+servidor.del( '/produtos/:idProd' , (req, res, next) => {
+    const idProduto = req.params.idProd;
+    knex('produto')
+        .where( 'id' , idProduto)
+        .delete()
+        .then( (dados) =>{
+            if( !dados ){
+                return res.send(
+                    new errors.BadRequestError('Produto não encontrado'));
+            }
+            res.send( "Produto Deletado" );
+        }, next) ; 
+});
+
+
+
